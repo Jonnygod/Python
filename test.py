848fc155a7de84765295a94cf12b6e5877ddca24
SimplePiano <45989845+Jonnygod@users.noreply.github.com>
2019-05-21 11:42:43 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,49 @@
+import json
+import requests
+import codecs
+
+headers = {
+    'Referer': 'https://www.xiaoheihe.cn/games/index',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'}
+# headers中的Referer参数是必须的，？号之前都是必须的后面可以省略，不会对结果有影响
+pagenum = 3
+# key = 'Python'  # 这里可以设置一个列表，先抓取页面所有的技术名称，保存起来，然后抓取职位信息的时候循环嵌套遍历
+json_url = 'https://api.xiaoheihe.cn/game/web/all_recommend/games/'
+score = ""
+
+
+# 获取json内容
+def get_content(post_data):
+    r = requests.get(json_url, headers=headers, params=post_data)
+    datass = json.loads(r.text)
+    datas = datass['result']["list"]
+    return datas
+
+def pipelines(datas):
+    with codecs.open('today.txt','a','utf-8') as fp:
+        for data in datas:
+            if data['name_en'] == "":
+                data['name_en'] = data['name']
+            if 'score' not in data.keys():
+                score = "暂无评分"
+            else:
+                score = data['score']
+            fp.write("游戏名：%s ,原价: %s元 ，现售价：%s元 折扣：-%s%% , 评分：%s ,史低：%s 元，图片: %s \n" %(data['name_en'],
+                                                                                   data['heybox_price']['original_coin']/1000,
+                                                                                   data['heybox_price']['cost_coin']/1000,
+                                                                                   data['heybox_price']['discount'],
+                                                                                   score,
+                                                                                   data['price']['lowest_price'],
+                                                                                   data['game_img']))
+
+
+
+# total_page = get_content(post_data)['total_page']  # 总页数
+# 循环每一页的内容
+for page in range(0, pagenum):
+    print(page+1)  # 记录当前页码
+    # offset:代表第几页
+    post_data = {'show_type': 'discount' , 'offset': page * 20 , 'limit': '20' ,'os_type': 'web','version':'999.0.0' }
+    datas = get_content(post_data)
+    pipelines(datas)
+
